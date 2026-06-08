ee0521b34d59052e80c6eea59d4cd0b83da17d63
methode <ujeanmethode@gmail.com>
2026-06-08 23:27:42 +0300

    FIX CONTRACT

--- admin/_dump_employee_header.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = $argv[1] ?? '';
-$z = new ZipArchive();
-$z->open($file);
-$x = $z->getFromName('word/document.xml');
-$z->close();
-$p = strpos($x, '>EMPLOYEE</w:t>');
-echo substr($x, $p, 2000);
--- admin/_list_placeholders.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = $argv[1] ?? '';
-$z = new ZipArchive();
-$z->open($file);
-$x = $z->getFromName('word/document.xml');
-$z->close();
-preg_match_all('/\$\{[a-z_]+\}/', $x, $m);
-echo basename($file) . ":\n" . implode(', ', array_unique($m[0])) . "\n";
